c345b346da6df28fd4af9456b7b8e8ad4bf1556f
Erickthe <erickouellette1105@gmail.com>
2025-11-13 18:32:08 -0500

    composante vue terminée

---
+++ projet-dnd/src/components/Inventory.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="inventory-wrapper">
+        <button @click="isOpen = true" class="btn-open">
+            îcone coffre
+        </button>
+
+        <!-- Inventaire -->
+         <div v-if="isOpen" class="inventory-modal">
+            <div class="inventory-content">
+                <h2>Inventaire</h2>
+
+                <ul>
+                    <li v-for="item in items" :key="item.id">
+                        {{ item.name }} - {{ item.img }}
+                    </li>
+                </ul>
+
+                <button @click="isOpen = false" class="btn-close">
+                    îcone coffre
+                </button>
+            </div>
+         </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isOpen: false,
+            items: [
+                { id: 1, name: "placeholder", img: ""},
+            ]
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped></style>
