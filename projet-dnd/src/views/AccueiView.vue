13e1f76fc43b83408a312f0685df46b681f29681
erickouellette1105 <erickouellette1105@gmail.com>
2025-11-14 12:15:49 -0500

    Ajout de nouveaux composants

---
+++ projet-dnd/src/views/AccueiView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1> Aventure de Stickman</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AccueilView',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped></style>
