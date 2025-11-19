4d743bec64810ed00d54a7e4ada34ee1220d0129
manelyaya <2375677@cmontmorency.qc.ca>
2025-11-19 13:04:39 -0500

    page sauvegarde

--- projet-dnd/src/views/SauvegardeView.vue
+++ projet-dnd/src/views/SauvegardeView.vue
@@ -58,11 +58,11 @@ export default {
     font-size: 100px;
     position: absolute;
     left: 50%;
-    transform: translateX(-50%); /* Centers correctly */
+    transform: translateX(-50%); 
     color: black;
 }
 
-/* Medium screens (tablets) */
+
 @media (max-width: 768px) {
     .save-header {
         font-size: 70px;
@@ -70,7 +70,7 @@ export default {
     }
 }
 
-/* Small screens (phones) */
+
 @media (max-width: 480px) {
     .save-header {
         font-size: 30px;
@@ -85,6 +85,8 @@ export default {
     display: grid;
     justify-content: center;
     gap: 30px;
+ 
+  
 }
 
 
