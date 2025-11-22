9e25e81fb3853d818f8afdcbbc035c8ea2cc71b2
Erickthe <erickouellette1105@gmail.com>
2025-11-22 13:51:30 -0500

    Inventaire styles

--- projet-dnd/src/components/AppHeaderGame.vue
+++ projet-dnd/src/components/AppHeaderGame.vue
@@ -3,7 +3,7 @@
         <img class="audio" src="../assets/img/audio_icon.png" alt="audio"></img>
 
         <!-- Bouton toggle de l'inventaire-->
-        <button @click="toggleInventory" class="btn-toggle"> 
+        <button @click="toggleInventory" class="btn-toggle">
             <img src="../assets/coffre.png" alt="coffre" class="coffre-img">
         </button>
 
@@ -13,17 +13,27 @@
             <img class="audio audio-in-modal" src="../assets/img/audio_icon.png" alt="audio"></img>
 
             <div class="inventory-content">
-                <div class="title-banner">
-                <h2>Inventaire</h2>
+
+                <div class="image-container-title">
+                    <img src="../assets/img/title-banner-inventory.png" alt="titre-banniere" class="titre-banner">
+                    <p class="text-titre">Inventaire</p>
                 </div>
                 <ul>
                     <li v-for="item in items" :key="item.id">
                         {{ item.name }}
                     </li>
                 </ul>
-                <div class="inventory-text">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+
+                <div class="image-container-text">
+                    <img src="../assets/img/text-banner-inventory.png" alt="texte-banniere" class="texte-banner">
+                    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id luctus odio.
+                        Curabitur vitae facilisis augue, ut convallis metus. Mauris convallis eros et lacus viverra
+                        facilisis. Nam sit amet turpis ex. Pellentesque habitant morbi tristique senectus et netus et
+                        malesuada fames ac turpis egestas. Ut leo urna, dapibus a pharetra sed, tempor id dui. Nullam
+                        pharetra velit id mi lacinia, nec congue nisl vestibulum. Suspendisse magna sapien, eleifend in
+                        facilisis id, sagittis eget massa. Aliquam erat volutpat. </p>
                 </div>
+
             </div>
         </div>
     </header>
@@ -99,17 +109,68 @@ export default {
 }
 
 .inventory-content {
-    color: white;
+    color: black;
+    height: 100%;
     display: grid;
     justify-content: center;
     align-items: center;
- 
+
+}
+
+.image-container {
+    position: relative;
+    display: inline-block;
+}
+
+.titre-banner {
+    width: 50vw;
+}
+
+.texte-banner {
+    width: 50vw;
+}
+
+.text {
+    position: absolute;
+    top: 20px;
+    left: 30px;
+}
+
+.image-container-title {
+    position: fixed;
+    top: 200px;
+    left: -7px;
+}
+
+.image-container-text {
+    position: fixed;
+    bottom: -13px;
+}
+
+.text {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 70%;
+    text-align: center;
+    color: white;
+    font-weight: bold;
+}
+
+.text-titre {
+    position: absolute;
+    top: 50%;
+    left: 70%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    color: white;
+    font-weight: bold;
+    font-size: 50px;
 }
 
 ul {
-       list-style-type: none;
+    list-style-type: none;
+    gap: 15px;
 }
-
-
-
 </style>
